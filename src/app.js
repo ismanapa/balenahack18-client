72043c8734578ba15a488b886b7a5c583af3dde4
ivilches@plainconcepts.com <ivilches@plainconcepts.com>
2018-10-20 13:22:03 +0200

    First test on device

---
+++ src/app.js
@@ -0,0 +1,7 @@
+
+var socket = require('socket.io-client')('http://10.10.1.88:8000');
+socket.on('connect', function(){ console.log('connected') });
+socket.on('event', function(data){ });
+socket.on('disconnect', function(){});
+
+socket.emit('chat message', 'hola que tal');
