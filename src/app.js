2579aa588b6cf8d025d43bca38a02a853ee1b0d9
ivilches@plainconcepts.com <ivilches@plainconcepts.com>
2018-10-20 13:24:41 +0200

    Message from device

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 
 var socket = require('socket.io-client')('http://10.10.1.88:8000');
-socket.on('connect', function(){ console.log('connected') });
+socket.on('connect', function(){ console.log('connected raspberry') });
 socket.on('event', function(data){ });
 socket.on('disconnect', function(){});
 
-socket.emit('chat message', 'hola que tal');
+socket.emit('chat message', 'hola hamijo, soy una raspi');
