e726529fa2bab973b556b00c6933a75b20cbf223
ivilches@plainconcepts.com <ivilches@plainconcepts.com>
2018-10-20 14:14:01 +0200

    Retrieving user data from server

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 
-var socket = require('socket.io-client')('http://10.10.1.88:8000');
-socket.on('connect', function(){ console.log('connected raspberry') });
-socket.on('event', function(data){ });
-socket.on('disconnect', function(){});
+const socket = require('socket.io-client')('http://10.10.1.95:8000');
 
-socket.emit('chat message', 'hola hamijo, soy una raspi');
+const SERVER_HOST = process.env.SERVER_HOST || 'http://10.10.1.95';
+const SERVER_PORT = process.env.SERVER_PORT || '8000';
+const MY_COLOR = process.env.MY_COLOR || [255, 255, 255];
+
+let userData = {
+    id: '',
+    position: {
+        x: 0,
+        y: 0
+    },
+    color: '',
+};
+
+socket.on('connect', () => { 
+    console.log('connected new player')
+});
+
+
+socket.on('start', function(newUser){
+    userData = Object.assign(userData, newUser);
+});
