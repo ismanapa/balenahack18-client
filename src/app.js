5f724def4f8f64c018cd925c3672d4e51bd8a51f
Christian Arenas <carenas@plainconcepts.com>
2018-10-21 13:38:54 +0200

    extract sensehat actions

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
-const senseLeds = require('sense-hat-led');
-const senseJoystick = require('sense-joystick');
-export { colors, pannels, config, settings, role, utils }
+
+export { colors, pannels, config, settings, role, utils, sensehat }
 
 const socket = require('socket.io-client')(`http://${SERVER_HOST}:${SERVER_PORT}`);
 
-//EVENTS
 socket.on('connect', () => { 
     console.log('connected new player');
     for(var second = 3; second >= 0; second--) {
         setInterval(function(){ 
-            senseLeds.setPixels(countDown[second]);
+            printPanel(countDown[second]);
         }, 1000);
     }
 });
 
 socket.on('disconnect', () => { 
-    senseLeds.setPixels(cross);
+    printPanel(cross);
 });
 
 socket.on('start', (newUser) => {
@@ -23,7 +21,7 @@ socket.on('start', (newUser) => {
     var newPanel = emptyMaze.slice(0);
     var position = GetPosition(player.position.x, player.position.y);
     newPanel[position] = getColor(player.role);
-    senseLeds.setPixels(newPanel);
+    printPanel(newPanel);
 });
 
 socket.on('update', (users) => {
@@ -34,34 +32,11 @@ socket.on('update', (users) => {
         newPanel[position] = color;
     })
 
-    senseLeds.setPixels(newPanel);
-});
-
-senseJoystick.getJoystick()
-.then((joystick) => {
-    joystick.on('press', (val) => {
-       var x = 0;
-       var y = 0;
-        switch(val) {
-           case 'up':
-               x = -1;
-            break;
-           case 'down':
-               x = 1;
-            break;
-           case 'left':
-               y = -1;
-            break;
-           case 'right':
-               y = 1;
-            break;
-       }
-       socket.emit("move", {id: player.id, move:{ x: x, y: y }})
-    });
+    printPanel(newPanel);
 });
 
 const start = () => {
-    senseLeds.setPixels(cross);
+    printPanel(cross);
 }
 
 start();
