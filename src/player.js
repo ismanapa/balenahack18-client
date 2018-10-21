0f1c02d3c4050a23744d85c15e1e056d5ff93742
Christian Arenas <carenas@plainconcepts.com>
2018-10-21 16:00:40 +0200

    remove properties

--- src/player.js
+++ src/player.js
@@ -2,11 +2,6 @@ var UserRole = require('./role');
 var MY_SURVIVOR_COLOR = require('./colors');
 
 class player {
-    id;
-    position;
-    color;
-    role;
-
     constructor() {
         this.id = 1;
         this.position = {x: 0, y: 0}
